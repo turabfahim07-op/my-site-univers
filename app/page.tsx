d8bbe069505dcc15e5fdf4447e1f3866905f8ea2
turabfahim07-op <turabfahim07@gmail.com>
2026-05-02 15:42:14 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      backgroundColor: '#202124', 
+      color: 'white', 
+      fontFamily: 'Arial' 
+    }}>
+      <h1>Sito di Fahim Online! 🚀</h1>
+    </div>
+  );
+}
