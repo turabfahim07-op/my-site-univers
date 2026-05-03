aad9d7eda2fc376b79fd9980821b763fc4f5e49b
turabfahim07-op <turabfahim07@gmail.com>
2026-05-03 16:16:40 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,4 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Fahim Search Engine",
-  description: "Il motore di ricerca dell'universo",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="it">
-      <body>{children}</body>
-    </html>
-  );
-} 
-export const metadata = {
+ export const metadata = {
   title: 'Fahim Search Engine',
   description: 'Il mio motore di ricerca personale',
 }
